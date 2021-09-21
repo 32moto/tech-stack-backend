9db823ffaaceefd4dc7ae15f3843ae8134ef043f
takuya wakazono <wakataku.11010809@gmail.com>
2021-09-21 22:16:39 +0900

    define user_images at ridgepole and generate rbs

--- app/models/user_image.rb
+++ app/models/user_image.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: user_images
+#
+#  id         :bigint           not null, primary key
+#  path       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
 class UserImage < ApplicationRecord
   belongs_to :company
 end
